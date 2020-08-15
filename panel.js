6ec5e795b6518e66f74cd8860e691f654e242b26
Karey Higuera <karey.higuera@gmail.com>
2020-08-15 14:16:59 -0500

    add new markdown processing function, partial

--- panel.js
+++ panel.js
@@ -13,8 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const convertToMarkdown = table => {
-  // if there's no node selected, hop out
-  if(!table){
+  // if there's no node selected or if it's not a table, hop out
+  if(!table || table.nodeName.toLowercase() === "table"){
     return false
   }
   let header = table.querySelector('thead')
@@ -36,5 +36,30 @@ const convertToMarkdown = table => {
     }
     markdown += '\n'
   }
+
+  let rows = [header_row, table_body.children]
+
+  let tableMatrix = rows.map(row => {
+    row.children.map(tableElement => tableElement.innerText)
+  })
+
+  // we'll let the first row determine our column count
+  let columnCount = tableMatrix[0].length
+
+  // create an array of 0s for current max width of each column
+  let columnWidths = Array(columnCount).fill(0)
+
+  // look at each row
+  for (row of tableMatrix){
+    // check each column of that row
+    for (let column = 0; column < columnCount; column++){
+      // if the length of this column item is the largest in its column
+      if (row[column].length > columnWidths[column]){
+        // set it as the new max column width
+        columnWidths[column] = row[column].length
+      }
+    }
+  }
+
   return markdown
 }
