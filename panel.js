8543d7ce551069f83dc7c4a2b9b365a9178b18fa
Karey Higuera <karey.higuera@gmail.com>
2020-08-14 20:35:25 -0500

    retrieve table as markdown from devtools context

--- panel.js
+++ panel.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#convert').addEventListener('click', () => {
     let textarea = document.querySelector('#markdown')
-    textarea.value = `Automatically set the textarea value!`
+    chrome.devtools.inspectedWindow.eval(`(${convertToMarkdown.toString()})($0)`, markdown => {
+      if(markdown){
+        textarea.value = markdown
+      } else {
+        // there was some sort of error
+        textarea.value = `There was an issue...`
+      }
+    })
   })
 })
 
 const convertToMarkdown = table => {
+  // if there's no node selected, hop out
+  if(!table){
+    return false
+  }
   let header = table.querySelector('thead')
   let header_row = header.querySelector('tr')
   let table_body = table.querySelector('tbody')
