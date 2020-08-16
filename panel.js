1b87b26a0ceb2d35766d520cd29a12bab2e6a13e
Karey Higuera <karey.higuera@gmail.com>
2020-08-15 22:12:30 -0500

    Refactor markdown creation to use a matrix (#1)
    * refactor markdown generator to pretty print

--- panel.js
+++ panel.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#convert').addEventListener('click', () => {
     let textarea = document.querySelector('#markdown')
-    chrome.devtools.inspectedWindow.eval(`(${convertToMarkdown.toString()})($0)`, markdown => {
-      if(markdown){
+    chrome.devtools.inspectedWindow.eval(`(${convertToMatrix.toString()})($0)`, matrix => {
+      if (matrix) {
+        let markdown = convertToMarkdown(matrix)
         textarea.value = markdown
       } else {
         // there was some sort of error
@@ -12,29 +13,64 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 })
 
-const convertToMarkdown = table => {
-  // if there's no node selected, hop out
-  if(!table){
+/**
+ * Returns a 2D array of the HTML table provided.
+ * @param {HTMLElement} table - the table to convert to a matrix.
+ */
+const convertToMatrix = table => {
+  // if there's no node selected or if it's not a table, hop out
+  if (!table || !table.nodeName.toLowerCase() === "table") {
     return false
   }
   let header = table.querySelector('thead')
   let header_row = header.querySelector('tr')
   let table_body = table.querySelector('tbody')
 
-  let markdown = '|'
+  // Combine header row and body rows into an array
+  let rows = [header_row, ...table_body.children]
 
-  for (const header_column of header_row.children) {
-    markdown += header_column.innerText + '|'
-  }
+  // map every item into a matrix
+  let tableMatrix = rows.map(row => [...row.children].map(tableElement => tableElement.innerText))
 
-  markdown += '\n|---|---|---|---|\n'
+  // let the first row (header) determine the column count
+  let columnCount = tableMatrix[0].length
 
-  for (let row of table_body.children) {
-    markdown += '|'
-    for (let column of row.children) {
-      markdown += column.innerText + '|'
+  // create an array for current max width of each column, default to 0
+  let columnWidths = Array(columnCount).fill(0)
+
+  // look at each row
+  for (row of tableMatrix) {
+    // check each column of that row
+    for (let column = 0; column < columnCount; column++) {
+      // if the length of this column item is the largest in its column
+      if (row[column].length > columnWidths[column]) {
+        // set it as the new max column width
+        columnWidths[column] = row[column].length
+      }
     }
-    markdown += '\n'
   }
+  return {
+    tableMatrix,
+    columnWidths
+  }
+}
+
+const convertToMarkdown = ({ tableMatrix, columnWidths }) => {
+  // TODO - allow for setting justification
+  let divider = columnWidths.map(columnWidth => '-'.repeat(columnWidth + 2))
+  // insert the divider after the header
+  tableMatrix.splice(1, 0, divider)
+
+  let markdown = tableMatrix.map(row => {
+    // create a row with all columns correctly spaced and joined together
+    let spacedRow = row.map((item, column) => {
+      let columnWidth = columnWidths[column] + 2
+      let leftSpace = ' '.repeat(Math.floor((columnWidth - item.length) / 2))
+      let rightSpace = ' '.repeat(Math.ceil((columnWidth - item.length) / 2))
+      return leftSpace + item + rightSpace
+    }).join('|')
+    return `|${spacedRow}|`
+  }).join('\n')
+
   return markdown
 }
